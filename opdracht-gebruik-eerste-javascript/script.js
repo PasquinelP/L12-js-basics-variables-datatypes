71e6ea9df0cb4064ae108f8212dc76ff0cb40290
Pascal <41127800+PasquinelP@users.noreply.github.com>
2021-01-26 21:53:56 +0100

    Add files via upload

---
+++ opdracht-gebruik-eerste-javascript/script.js
@@ -0,0 +1,28 @@
+console.log("Hello Winc Academy");
+
+let naam = "Pascal";
+console.log(naam);
+
+let numbers = 4 + 4;
+console.log(numbers);
+
+let strings = "4" + "4";
+console.log(strings);
+
+let subtract = 4 - 1;
+console.log(subtract);
+
+let multiply = 2 * 5;
+console.log(multiply);
+
+let division = 10 / 2;
+console.log(division);
+
+let modulus = 3 % 2;
+console.log(modulus);
+
+let age = 30;
+console.log(typeof age);
+
+let age2 = "30";
+console.log(typeof age2);
