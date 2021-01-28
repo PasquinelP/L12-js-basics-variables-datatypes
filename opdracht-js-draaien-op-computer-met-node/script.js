615d3240598d8a9afeef229c70636c08c8e90727
Pascal <41127800+PasquinelP@users.noreply.github.com>
2021-01-28 16:05:13 +0100

    Add files via upload

---
+++ opdracht-js-draaien-op-computer-met-node/script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Winc Academy studenten");
+
+/*
+// Dit is een grote som
+let calculate = 1230941 * 1823794;
+console.log(calculate);
+
+let divide = 637263 / 54;
+console.log(divide);
+*/
